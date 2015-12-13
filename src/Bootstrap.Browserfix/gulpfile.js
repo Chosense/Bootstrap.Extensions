11e9e3c9773367449019532b27868727dbeea4dd
Mika Berglund <mika.berglund@develore.com>
2015-12-13 20:53:39 +0200

    Added the dev project and build tasks to produce the dist folder.

---
+++ src/Bootstrap.Browserfix/gulpfile.js
@@ -0,0 +1,46 @@
+﻿/// <binding BeforeBuild='beforeBuild' AfterBuild='afterBuild' Clean='clean' ProjectOpened='default' />
+"use strict";
+
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var cssmin = require("gulp-cssmin");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var del = require("del");
+
+gulp.task("default", function () {
+    gulp.watch("./Sass/**/*.scss", ["compile:sass"]);
+});
+
+gulp.task("clean:css", function () {
+    return del("./wwwroot/css/**/*");
+});
+gulp.task("clean:dist", function () {
+    return del("../../dist/**/*", { force: true });
+});
+
+
+gulp.task("compile:sass", function () {
+    gulp.src("./Sass/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./wwwroot/css/"))
+        .pipe(cssmin())
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(gulp.dest("./wwwroot/css/"));
+});
+
+gulp.task("copy:css", function () {
+    gulp.src("./wwwroot/css/**/*")
+        .pipe(gulp.dest("../../dist/css/"));
+});
+
+
+
+
+
+gulp.task("clean", ["clean:css", "clean:dist"]);
+gulp.task("beforeBuild", ["compile:sass"]);
+gulp.task("afterBuild", ["copy:css"]);
---
+++ src/Bootstrap.Browserfix/wwwroot/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="js/site.js" />
+/// <reference path="lib/bootstrap-sass-official/assets/javascripts/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
